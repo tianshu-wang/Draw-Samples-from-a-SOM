c7365ac0ae71f85107d627ac50083591f09a4bea
tianshu-wang <31485874+tianshu-wang@users.noreply.github.com>
2020-08-11 15:22:52 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -32,14 +32,15 @@ print(np.average(target))
 
 # Load SOM
 size = 40
-
 som = som.MiniSom(size,size,len(data[0]),sigma=1,learning_rate=0.1)
 som._weights = np.load('som-%d.npy'%size)
+
+# Load averaged utility in each cell and flatten it.
 utils_weights = np.load('utils-%d.npy'%size)
 weights = utils_weights.flatten() # cell (x,y) or SOM[y,x] is now mapped to index=size*y+x
 
-
-distribution = [[]] # Put all data points (their indexs) into each cells, so that they can be selected later. 
+# Put all data points (their indexs) into each cells, so that they can be selected later. 
+distribution = [[]] 
 for i in range(size**2-1):
     distribution = distribution+[[]] 
 for cnt,xx in enumerate(data):
@@ -50,7 +51,7 @@ for cnt,xx in enumerate(data):
 chosen_bins = np.random.choice(range(size**2),100000,p=weights/np.sum(weights)) #weighted
 #chosen_bins = np.random.choice(range(size**2),20000) #uniformly
 
-# Draw samples from each cell, according to the number that the cell is called in chosen_bins
+# Then draw samples from each cell, according to the number that the cell is called in chosen_bins
 chosen = []
 for i in range(size**2):
     chosen.append(np.random.choice(distribution[i],np.count_nonzero(chosen_bins==i)))
